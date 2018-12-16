5f3fe24c7cc5d8c8230c47e443a08e74a7ccbf72
Vera Cat <vdikun@hotmail.com>
2018-12-16 02:10:35 -0800

    moved handleChange function into separate file

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,47 +1,7 @@
 import { createStore } from 'redux';
-import { AppState } from 'react-native';
 import reducers from './../reducers';
-import { togglePomodoro } from './../actions';
-import Sounds from './../../utils/sound';
-import pomodoroState from '../state';
-
-import NotificationService from '../../notificationService';
-
-const { ting, blop } = Sounds;
-
-const { popNotif } = NotificationService;
+import handleChange from './../handleChange';
 
 export default store = createStore(reducers);
-
-const handleChange = () => {
-  let currentValue = store.getState();
-  let pomodoro = currentValue.pomodoro;
-  if (pomodoro.secondsRemaining == 0) {
-    let sound, message;
-    if (pomodoro.pomodoroState == pomodoroState.WORK) {
-      sound = ting;
-      message = "Break time!"
-    } else {
-      sound = blop;
-      message = "Back to work!"
-    }
-    if (AppState.currentState.match(/inactive|background/)) {
-      // show notification
-      popNotif(message);
-    }
-    // play sound
-    sound.play((success) => {
-      if (success) {
-        console.log('successfully finished playing');
-      } else {
-        console.log('playback failed due to audio decoding errors');
-        // reset the player to its uninitialized state (android only)
-        // this is the only option to recover after an error occured and use the player again
-        sound.reset();
-      }
-    });
-    store.dispatch(togglePomodoro());
-  }
-};
 
 export const unsubscribe = store.subscribe(handleChange);
