48f983419748e94c5373761cb1ee206a05239bee
Vera Cat <vdikun@hotmail.com>
2018-12-21 11:34:19 -0800

    close settings modal when tapping outside of modal

--- src/components/controls/SettingsControl/SettingsModal/index.js
+++ src/components/controls/SettingsControl/SettingsModal/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { Modal, Text, View } from 'react-native';
+import { Text, TouchableWithoutFeedback, View } from 'react-native';
+import Modal from 'react-native-modal';
 import { connect } from 'react-redux';
 import IntervalInput from './IntervalInput';
 import Button from './../../../common/Button';
@@ -17,42 +18,45 @@ class SettingsModal extends Component {
     render () {
         const { visible, onClose, onSave } = this.props;
         return (
-            <Modal
-            animationType="slide"
-            transparent={true}
-            visible={visible}
-            onRequestClose={() => onClose()}>
-                <View style={styles.modalContent}>
-                    <View style={styles.view} >
-                        <Text style={styles.header}>Settings</Text>
-                        <View style={styles.setting}>
-                            <IntervalInput
-                                title={"Work time interval"} 
-                                initValue={this.state.workMinutes}
-                                onChange={value => this.setState({workMinutes: value})} />
+            <TouchableWithoutFeedback onPress={() => onClose()}>
+                <Modal
+                style={{margin: 0}}
+                animationType="slide"
+                transparent={true}
+                isVisible={visible}
+                onRequestClose={() => onClose()}>
+                    <View style={styles.modalContent}>
+                        <View style={styles.view} >
+                            <Text style={styles.header}>Settings</Text>
+                            <View style={styles.setting}>
+                                <IntervalInput
+                                    title={"Work time interval"} 
+                                    initValue={this.state.workMinutes}
+                                    onChange={value => this.setState({workMinutes: value})} />
+                            </View>
+                            <View style={styles.setting}>
+                                <IntervalInput
+                                    title={"Break time interval"} 
+                                    initValue={this.state.breakMinutes}
+                                    onChange={value => this.setState({breakMinutes: value})} />
+                            </View>
+                            <View style={styles.setting}>
+                                <Text>Tone played when break starts</Text>
+                            </View>
+                            <View style={styles.setting}>
+                                <Text>Tone played when break ends</Text>
+                            </View>
+                            <Button text="Save" onPress={() => {
+                                onClose();
+                                onSave({
+                                    workMinutes: this.state.workMinutes,
+                                    breakMinutes: this.state.breakMinutes
+                                });
+                                }} />
                         </View>
-                        <View style={styles.setting}>
-                            <IntervalInput
-                                title={"Break time interval"} 
-                                initValue={this.state.breakMinutes}
-                                onChange={value => this.setState({breakMinutes: value})} />
-                        </View>
-                        <View style={styles.setting}>
-                            <Text>Tone played when break starts</Text>
-                        </View>
-                        <View style={styles.setting}>
-                            <Text>Tone played when break ends</Text>
-                        </View>
-                        <Button text="Save" onPress={() => {
-                            onClose();
-                            onSave({
-                                workMinutes: this.state.workMinutes,
-                                breakMinutes: this.state.breakMinutes
-                            });
-                            }} />
                     </View>
-                </View>
-            </Modal>
+                </Modal>
+            </TouchableWithoutFeedback>
         );
     }
 };
